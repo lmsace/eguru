853d068c5b61a360105f3488bf4d6b6afd3f0c97
Nithyanandan-lmsace <nithyanandan.lmsace@gmail.com>
2022-05-16 15:02:09 +0530

    version and readme file updated

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 // The current module version (Date: YYYYMMDDXX).
-$plugin->version   = 2022051400;
+$plugin->version   = 2022051600;
 
 
 // Version's maturity level.
