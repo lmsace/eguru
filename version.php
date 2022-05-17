10f9f42642f6555e5c8665a98624fcea83f0c023
vasanthlmsace <vasanthr.lmsace@gmail.com>
2022-05-17 15:03:07 +0530

    version updated

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 // The current module version (Date: YYYYMMDDXX).
-$plugin->version   = 2022051600;
+$plugin->version   = 2022051700;
 
 
 // Version maturity level.
