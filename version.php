06353f29383f9037b2b4798ff1b18182a36ce170
Nithy1anandan <nithyanandan.m.2016@lmsace.com>
2023-01-19 18:48:29 +0530

    version file update

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 // The current module version (Date: YYYYMMDDXX).
-$plugin->version   = 2023011903;
+$plugin->version   = 2023011900;
 
 // Version maturity level.
 $plugin->maturity = MATURITY_STABLE;
